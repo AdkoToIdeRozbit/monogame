94564c36a3733b0bf0524bfe52ad93e96050d9f5
Adrian Hochla <92049048+AdkoToIdeRozbit@users.noreply.github.com>
2025-12-11 08:15:27 +0100

    collision detection basic

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { TransformControls } from "three/addons/controls/TransformControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 let cameraPersp: any, cameraOrtho: any, currentCamera: any;
-let scene: any, renderer: any, control: any, orbit: any, loader: GLTFLoader;
+let scene: any, renderer: any, control: TransformControls, control2: TransformControls, orbit: any, loader: GLTFLoader;
 
 init();
 render();
@@ -47,15 +47,68 @@ async function init() {
     orbit.enabled = !event.value;
   });
 
-  const gltf = await loader.loadAsync("models/monotile.glb");
-  gltf.scene.scale.set(0.05, 0.05, 0.05);
-  gltf.scene.rotation.set(-Math.PI / 2, 0, 0);
-  scene.add(gltf.scene);
+  control2 = new TransformControls(currentCamera, renderer.domElement);
+  control2.addEventListener("change", render);
+  control2.addEventListener("dragging-changed", function (event: any) {
+    orbit.enabled = !event.value;
+  });
 
-  control.attach(gltf.scene);
+  const gltf = await loader.loadAsync("models/monotile.glb");
+  const tile = gltf.scene;
+  const tile2 = tile.clone();
+
+  tile.scale.set(0.05, 0.05, 0.05);
+  tile.rotation.set(-Math.PI / 2, 0, 0);
+
+  tile2.scale.set(0.05, 0.05, 0.05);
+  tile2.rotation.set(-Math.PI / 2, 0, 0);
+  tile2.position.set(0, 0, 3);
+
+  scene.add(tile);
+  scene.add(tile2);
+
+  control.showY = false;
+  control2.showY = false;
+
+  control.attach(tile);
+  control2.attach(tile2);
 
   const gizmo = control.getHelper();
+  const gizmo2 = control2.getHelper();
+
   scene.add(gizmo);
+  scene.add(gizmo2);
+
+  let previousMatrix1 = tile.matrix.clone();
+  let previousMatrix2 = tile2.matrix.clone();
+
+  function checkCollisionForTile1() {
+    const box1 = new THREE.Box3().setFromObject(tile);
+    const box2 = new THREE.Box3().setFromObject(tile2);
+    if (box1.intersectsBox(box2)) {
+      tile.matrix.copy(previousMatrix1);
+      tile.matrix.decompose(tile.position, tile.quaternion, tile.scale);
+      tile.updateMatrixWorld();
+    } else {
+      previousMatrix1 = tile.matrix.clone();
+    }
+    render();
+  }
+
+  function checkCollisionForTile2() {
+    const box1 = new THREE.Box3().setFromObject(tile);
+    const box2 = new THREE.Box3().setFromObject(tile2);
+    if (box1.intersectsBox(box2)) {
+      tile2.matrix.copy(previousMatrix2);
+      tile2.matrix.decompose(tile2.position, tile2.quaternion, tile2.scale);
+      tile2.updateMatrixWorld();
+    } else {
+      previousMatrix2 = tile2.matrix.clone();
+    }
+    render();
+  }
+  control.addEventListener("objectChange", checkCollisionForTile1);
+  control2.addEventListener("objectChange", checkCollisionForTile2);
 
   window.addEventListener("resize", onWindowResize);
 }
